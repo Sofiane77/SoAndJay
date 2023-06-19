6e1c3f51b116a032e88a063210c027000f242c62
Sofiane77 <sbelhadi@atakama_technologies.com>
2023-06-19 10:55:23 +0200

    Initial commit

---
+++ So-Jay/src/main/java/com/doranco/SoAndJay/SoAndJayApplication.java
@@ -0,0 +1,13 @@
+package com.doranco.SoAndJay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoAndJayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoAndJayApplication.class, args);
+	}
+
+}
---
+++ So-Jay/src/test/java/com/doranco/SoAndJay/SoAndJayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doranco.SoAndJay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoAndJayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
