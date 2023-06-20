61e0b8e96d5e41c5b49d4acd1afd29fee0cca782
JaysonL3M <jayson@l3m-holding.fr>
2023-06-20 15:08:32 +0200

    test controller Home

---
+++ So-Jay/src/main/java/com/doranco/SoAndJay/restController/controllerVue/HomeController.java
@@ -0,0 +1,13 @@
+package com.doranco.SoAndJay.restController.controllerVue;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String accueil() {
+        return "accueil";
+    }
+}
