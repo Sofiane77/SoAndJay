79d7d3a08510beb9746cb4ab52caa80d8700b490
JaysonL3M <jayson@l3m-holding.fr>
2023-06-20 10:45:10 +0200

    tzest

--- So-Jay/src/main/java/com/doranco/SoAndJay/repository/UserRepository.java
+++ So-Jay/src/main/java/com/doranco/SoAndJay/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.doranco.SoAndJay.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.doranco.SoAndJay.entities.Utilisateur;
@@ -7,3 +8,4 @@ import com.doranco.SoAndJay.entities.Utilisateur;
 public interface UserRepository extends JpaRepository<Utilisateur, Long> {
     // Méthodes spécifiques à ajouter si nécessaire
 }
+
