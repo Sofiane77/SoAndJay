fad9f606fc99d9c913d255eed085a6939a68434c
JaysonL3M <jayson@l3m-holding.fr>
2023-06-20 09:41:31 +0200

    Modif Repo et Controller

--- So-Jay/src/main/java/com/doranco/SoAndJay/repository/ArticlePanierRepository.java
+++ So-Jay/src/main/java/com/doranco/SoAndJay/repository/ArticlePanierRepository.java
@@ -1,10 +1,13 @@
 package com.doranco.SoAndJay.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.doranco.SoAndJay.entities.ArticlePanier;
 
+@Repository
 public interface ArticlePanierRepository extends JpaRepository<ArticlePanier, Long> {
-    // Méthodes spécifiques à ajouter si nécessaire
+    List<ArticlePanier> findByUtilisateurId(Long utilisateurId);
 }
-
